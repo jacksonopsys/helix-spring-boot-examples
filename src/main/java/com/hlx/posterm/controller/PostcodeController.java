fdde29141319e6803119159581c7257a16d92bc5
Matthew <matthew.jackson@usa.net>
2021-08-17 21:22:54 -0500

    Preconditions first lab

---
+++ src/main/java/com/hlx/posterm/controller/PostcodeController.java
@@ -0,0 +1,29 @@
+package com.hlx.posterm.controller;
+
+import com.hlx.posterm.dto.PostCodeInfo;
+import com.hlx.posterm.dto.ValidationStatus;
+import com.hlx.posterm.service.PostCodeValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/address")
+public class PostcodeController {
+
+    private PostCodeValidationService svc;
+
+    public PostcodeController(@Autowired PostCodeValidationService svcIn) {
+        svc = svcIn;
+    }
+
+    @PostMapping
+    public ValidationStatus doPost(@RequestBody PostCodeInfo postCodeInfoIn) {
+        return svc.validate(postCodeInfoIn);
+    }
+
+    @GetMapping("/{localeIn}/{postCodeIn}")
+    public ValidationStatus doGet(@PathVariable String postCodeIn, @PathVariable String localeIn) {
+        return svc.validate(PostCodeInfo.builder().locale(Locale.forLanguageTag(localeIn)).code(postCodeIn).build());
+    }
+}
